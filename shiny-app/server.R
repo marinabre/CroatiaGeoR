e3d1ed699164d863b07919677d79eed39756a5c1
Marina Brebric <marina.brebric@fer.hr>
2017-04-14 22:37:00 +0200

    started making shiny app - for now I have managed to show counties on OpenStreetMap Tile, dynamical changing of data shown (file from which data is pulled, year of data, color pallete used)

---
+++ shiny-app/server.R
@@ -0,0 +1,75 @@
+
+
+server <- function(input, output, session) {
+  source("global.R")
+
+  podaci_input <- reactive({
+    podaci <- read.csv(input$izvor, stringsAsFactors = F)
+    #izbacivanje RH
+    podaci <- podaci[-1,]
+    names(podaci)[1] <- "LOCALNAME"
+    podaci <- podaci[order(podaci$LOCALNAME),]
+    rownames(podaci) <- NULL
+    podaci
+  })
+    
+    
+  dat_source <- reactive({
+    merge(zupanije_RH, podaci_input(), by="LOCALNAME")
+  })
+  
+  colorpal <- reactive({
+    colorNumeric(input$colors, NULL)
+  })
+  
+  odabrana_godina <- reactive({
+    dat_source()@data[,input$godine]
+  })
+  
+  godina_text <- reactive({
+    gsub("X", "", input$godine)
+  })
+  
+  legend_title <- reactive({
+    paste("Ukupni broj prometnih nesreća u", godina_text()) 
+  })
+    
+  output$mymap <- renderLeaflet({
+    leaflet(data = dat_source(), options = leafletOptions(minZoom = 7, maxZoom = 10)) %>% addTiles() %>%
+      setView(zoom = 7, lng = 17, lat=44) %>%
+      fitBounds(lat1=42, lng1=15, lat2=46, lng2=20) %>%
+      addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
+                  opacity = 1.0, fillOpacity = 0.5, fillColor = ~colorpal()(odabrana_godina()),
+                  label = ~paste0(LOCALNAME, ": ", formatC(odabrana_godina(), big.mark = ".", decimal.mark=",")),
+                  highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                      bringToFront = TRUE)) %>%
+      addLegend(position = "bottomright", pal = colorpal(), 
+                values = ~log10(odabrana_godina()), title=legend_title(), 
+                opacity = 1.0, labFormat = labelFormat(transform = function(x) round(10^x)))
+  })
+  
+  observe({
+    
+    change <- input$izvor
+    
+    if(change != "./data/transport broj prometnih nesreca ukupno.csv"){
+      
+      updateSelectInput(session, "godine",
+                        label = "Godine podataka",
+                        choices = names(podaci_input())[-(1:2)], selected = "X2014."
+      )
+    }
+    data <- dat_source()
+    leafletProxy("mymap", data = data)  %>% clearControls() %>% clearShapes() %>%
+      addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
+                  opacity = 1.0, fillOpacity = 0.5, fillColor = ~colorpal()(odabrana_godina()),
+                  label = ~paste0(LOCALNAME, ": ", formatC(odabrana_godina(), big.mark = ".", decimal.mark=",")),
+                  highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                      bringToFront = TRUE)) %>%
+      addLegend(position = "bottomright", pal = colorpal(), 
+                values = ~log10(odabrana_godina()), title=legend_title(), 
+                opacity = 1.0, labFormat = labelFormat(transform = function(x) round(10^x)))
+    
+  })
+  
+}
---
+++ shiny-app/ui.R
@@ -0,0 +1,57 @@
+
+header <- dashboardHeader(title = "Analiza statistike RH")
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Podaci na karti RH", tabName = "kartaRH", icon = icon("map")),
+    menuItem("Pojedina županija", tabName = "zupanija", icon = icon("location-arrow")),
+    menuItem("Podaci prikazani grafom", tabName = "grafRH", icon = icon("bar-chart")),
+    menuItem("Unos vlastitih podataka", tabName = "upload", icon = icon("cloud-upload"))
+  )
+)
+
+body <- dashboardBody(
+  tabItems(
+    # Main tab
+    tabItem(tabName = "dashboard",
+            h1("Moja aplikacija"),
+            p("opis moje aplikacije")
+    ),
+    # First tab content
+    tabItem(tabName = "kartaRH",
+            fluidRow(
+              selectInput("izvor", "Izvor podataka", lista_izvora, selected = "./data/transport broj prometnih nesreca ukupno.csv"),
+              div(class="outer",
+                  tags$style(type = "text/css", ".outer {position: fixed; top: 150px; left: 0; right: 0; bottom: 0; overflow: hidden; padding: 0, margin: 0 auto;}"),
+                  leafletOutput("mymap", width = "100%", height = "90%")
+                  ),
+              absolutePanel(top = 50, right = 10,
+                            selectInput("colors", "Color Scheme",
+                                        rownames(subset(brewer.pal.info, category %in% c("seq", "div"))), selected = "Reds"
+                            ),
+                            selectInput("godine", "Godine podataka",
+                                        inicijalne_godine, selected = "X2014.")
+              )
+            )
+            
+    ),
+    
+    # Second tab content
+    tabItem(tabName = "zupanija",
+            h2("Županija brrrm")
+    ),
+    
+    # Third tab content
+    tabItem(tabName = "grafRH",
+            h2("Grafićiiiii")
+    ),
+    # Fourth tab content
+    tabItem(tabName = "upload",
+            h2("Učitvanje vlastitog seta podataka za prikaz nad kartom RH")
+    )
+  )
+)
+
+
+ui <- dashboardPage(header, sidebar, body)
